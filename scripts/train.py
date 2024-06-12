c53f75e6bfdd81ce542f5f4f6cfe93a2912b44fb
Cristian Cardellino <crscardellino@gmail.com>
2024-06-12 10:56:08 +0200

    Added flag for add_prefix_space

--- scripts/train.py
+++ scripts/train.py
@@ -282,6 +282,9 @@ if __name__ == "__main__":
                         action="store_true",
                         help="Only useful for Sequence Tagging trainings. "
                              "If true the loss function uses Conditional Random Fields.")
+    parser.add_argument("--add-prefix-space",
+                        action="store_true",
+                        help="Activate Roberta based models.")
     parser.add_argument("--log-every-n-steps",
                         default=50,
                         type=int,
@@ -353,7 +356,7 @@ if __name__ == "__main__":
             cache_dir=config.cache_dir,
             do_lower_case=config.lower_case,
             use_fast=True,
-            add_prefix_space=True if hf_tokenizer_name_or_path == 'roberta-base' else False
+            add_prefix_space=config.add_prefix_space
         ),
         datasets_config=dict(
             max_seq_length=config.max_seq_length
